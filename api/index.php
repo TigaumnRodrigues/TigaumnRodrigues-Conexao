e906f712a0c9c614a880831168b40d5a2a26b213
TigaumnRodrigues <tigaumn@gmail.com>
2023-11-30 14:51:58 -0300

    Initial commit

---
+++ api/index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo '{
+    "produtos": [
+      {
+        "nome": "Produto teste",
+        "img": "/img/sw1.png",
+        "preco": "R$100,00",
+        "id": 9999999,
+        "alt": "Produto",
+        "section": "Star Wars",
+        "descricao": "esse é um produto teste."
+      }
+      ]
+      }';
+
+?>
+
